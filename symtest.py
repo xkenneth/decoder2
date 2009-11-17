771ad3796f2dd1e3ba719b9e9885705854ebaad1
Kenneth Miller <xkenneth@ubuntu.(none)>
2009-11-16 19:25:52 -0800

    Got something working.

--- symtest.py
+++ symtest.py
@@ -1,5 +1,10 @@
+#!/usr/bin/env python
+
 import pdb
 import math
+import pylab
+import random
+
 #half time
 narrow = 1.0
 wide = 2.0
@@ -62,12 +67,40 @@ def mean(numberList):
     floatNums = [float(x) for x in numberList]
     return sum(floatNums) / len(numberList)
 
+def add_noise(signal,amplitude=0.3):
+    return [ mod + random.uniform(-amplitude,amplitude) for mod in signal ]
+    
+    
+noisy_signal = add_noise(symbol_00,0.5)
+perfect_signal = compute_signal(symbols[0],symbols[0])
 
-data = [ sum(signal_error(diff_signal(symbol_00,sym))) for sym in all_symbols]
+data = [ sum(signal_error(diff_signal(noisy_signal,sym))) for sym in all_symbols]
 
 print "Min:", min(data)
 print "Max:", max(data)
 print "Mean:", mean(data)
+print "STD:", pylab.std(data)
+print "Noisy Signal:",noisy_signal
+print "Correct Pattern:", perfect_signal
+print "Correct Difference:", diff_signal(noisy_signal,perfect_signal)
+print "Correct Error:", signal_error(diff_signal(noisy_signal,perfect_signal))
+print "Correct Sum:", sum(signal_error(diff_signal(noisy_signal,perfect_signal)))
+
 data.sort()
 print "Sort:"
 print data
+#opylab.hist(data,bins=30)
+
+
+
+for i in range(100):
+    stds = []
+    noisy_signal = add_noise(symbol_00,1.0/(float(i)+1.0))
+    data = [ sum(signal_error(diff_signal(noisy_signal,sym))) for sym in all_symbols]
+    stds.append(pylab.std(data))
+
+pylab.hist(stds,bins=30)
+
+pylab.show()
+    
+    
